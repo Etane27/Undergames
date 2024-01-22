3b88fe82f2f0d852d2806a8b955b5126db4b9984
etane27 <etane.babel@gmail.com>
2024-01-22 21:07:54 +0100

    commit avec le menu theme

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-import pendu 
-import juste_prix
 import pygame
 import sys 
 import os
-#configuration de la fenêtre principale 
+
+# Configuration de la fenêtre Pygame
 pygame.init()
-taille = width,height = 1920,1080
+taille = width, height = 1920, 1080
 ecran = pygame.display.set_mode(taille)
+pygame.display.set_caption("Menu Principal")
+
+# Chargement de l'image de fond
 background_path = os.path.join("Undergames", "sprites", "menu_background.gif")
 background = pygame.image.load(background_path)
-background = pygame.transform.scale(background,taille)
+background = pygame.transform.scale(background, taille)
 
-#chargement logo
-logo_path = os.path.join("Undergames","sprites","logo_menu.png")
+# Chargement du logo
+logo_path = os.path.join("Undergames", "sprites", "logo_menu.png")
 logo = pygame.image.load(logo_path)
-logo_position = ((width - logo.get_width()) // 2,100)
+logo_position = ((width - logo.get_width()) // 2, 100)
 
+# Chargement de la musique
+musiques_path = "C:\\Users\\etane\\OneDrive\\Documents\\PROGRAMES\\UNDERGAMES\\Undergames\\sounds\\menu_theme.mp3"
+pygame.mixer.music.load(musiques_path)
 
 # Boucle principale Pygame
+pygame.mixer.music.play(loops=-1)
 running = True
 while running:
+    if not pygame.mixer.music.get_busy():
+        pygame.mixer.music.load('C:\\Users\\etane\\OneDrive\\Documents\\PROGRAMES\\UNDERGAMES\\Undergames\\sounds\\menu_theme.mp3')
+        pygame.mixer.music.play()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -28,10 +37,13 @@ while running:
 
     # Dessiner l'arrière-plan
     ecran.blit(background, (0, 0))
-    ecran.blit(logo,logo_position)
+    ecran.blit(logo, logo_position)
 
     # Mettre à jour l'affichage
     pygame.display.flip()
+
+# Arrêter la musique avant de quitter
+pygame.mixer.music.stop()
 
 # Quitter Pygame
 pygame.quit()
